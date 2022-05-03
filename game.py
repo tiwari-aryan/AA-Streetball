6394bfe55b00b9aa0d335f451b019a8826e9edbd
lol troll21 <sparklingaryan@gmail.com>
2022-05-03 00:16:15 -0400

    Made some changes to main to get ready to add a how to play section, also fixed up music player issues

--- game.py
+++ game.py
@@ -27,8 +27,8 @@ title_3 = title_font.render("AA Streetball", False, (13, 20, 211))
 p1_game_win_text = font.render("Player 1 Won!", False, (255, 255, 100))
 p2_game_win_text = font.render("Player 2 Won!", False, (255, 255, 100))
 court = pygame.image.load("images/Court.png")
-x = pygame.image.load("images/pascal.png")
-o = pygame.image.load("images/scottie.png")
+pascal = pygame.image.load("images/pascal.png")
+scottie = pygame.image.load("images/scottie.png")
 
 
 def main():
@@ -36,15 +36,17 @@ def main():
     start_y = 0
     i = 1
     title_count = 1
-    p1 = Player(774, 598, x, 6, 6, 50, 100, True, 1)
-    p2 = Player(874, 543, o, 6, 6, 50, 100, False, 2)
+    p1 = Player(774, 598, pascal, 6, 6, 50, 100, True, 1)
+    p2 = Player(874, 543, scottie, 6, 6, 50, 100, False, 2)
     p1.player_movement = 1
     p2.player_movement = 2
     keys = pygame.key.get_pressed()
     run = True
     in_title = True
+    in_team = False
     in_options = False
     in_music = False
+    in_how_to_play = False
     in_game = False
     slider_x = 920
     slider_y = 50
@@ -59,12 +61,6 @@ def main():
     song_number = 0
     skip_timer = 0
     rewind_timer = 0
-    skip_song = Button(1020, 900, 1475, 980, False)
-    rewind_song = Button(525, 900, 1035, 980, False)
-    go_back = Button(27, 20, 484, 160, False)
-    volume_slider = Slider(
-        slider_x, slider_y, slider_x + 10, slider_y + 10, False, False
-    )
     while run:
         keys = pygame.key.get_pressed()
         clock.tick(60)
@@ -84,8 +80,8 @@ def main():
             title_count += 1
             start_x += i
             start_y += i
-            win.blit(x, (160 + start_x, 100 + start_y**2))
-            win.blit(o, (160 + start_x, 850 + start_y**2))
+            win.blit(pascal, (160 + start_x, 100 + start_y**2))
+            win.blit(scottie, (160 + start_x, 850 + start_y**2))
             if title_count <= 20:
                 win.blit(title_1, (400, 400))
             elif title_count <= 40:
@@ -99,26 +95,45 @@ def main():
             if keys[pygame.K_RETURN]:
                 in_options = True
                 in_title = False
-
+        elif in_team == True:
+            win.blit(pascal, (700, 250))
+            win.blit(scottie, (700, 830))
+            if keys[pygame.K_RETURN]:
+                in_game = True
+                in_team = False
+            pygame.display.update()
+            pygame.display.flip()
         elif in_options == True:
 
             play_game = Button(728, 385, 1185, 515, False)
             music = Button(728, 558, 1185, 695, False)
+            how_to_play = Button(728, 731, 1185, 775, False)
 
             play_game.on_click()
             music.on_click()
+            how_to_play.on_click()
             if play_game.click:
-                in_game = True
+                in_team = True
                 in_options = False
 
             if music.click:
                 in_music = True
+                in_options = False
+
+            if how_to_play.click:
+                in_how_to_play = True
                 in_options = False
             win.blit(pygame.image.load("images/Home_Screen.png"), (0, 0))
             play_game.draw(win)
             music.draw(win)
 
         elif in_music:
+            skip_song = Button(1020, 900, 1475, 980, False)
+            rewind_song = Button(525, 900, 1035, 980, False)
+            go_back = Button(27, 20, 484, 160, False)
+            volume_slider = Slider(
+                slider_x, slider_y, slider_x + 10, slider_y + 10, False, False
+            )
             win.blit(pygame.image.load("images/Music_Screen.png"), (0, 0))
             win.blit(song_image[song_number], (718, 259))
 
@@ -130,6 +145,7 @@ def main():
             if go_back.click:
                 in_options = True
                 in_music = False
+                go_back.click = False
             volume_slider.draw(win)
             skip_song.on_click()
             rewind_song.on_click()
@@ -158,45 +174,45 @@ def main():
             if p1.reset_possession:
                 p1.reset_possession = False
                 placement_score = p1.score
-                p1 = Player(774, 598, x, 6, 6, 50, 100, True, 1)
+                p1 = Player(774, 598, pascal, 6, 6, 50, 100, True, 1)
                 p1.score = placement_score
                 placement_score = p2.score
-                p2 = Player(874, 543, o, 6, 6, 50, 100, False, 2)
+                p2 = Player(874, 543, scottie, 6, 6, 50, 100, False, 2)
                 p2.score = placement_score
             if p2.reset_possession:
                 p2.reset_possession = False
                 placement_score = p1.score
-                p1 = Player(874, 598, x, 6, 6, 50, 100, False, 1)
+                p1 = Player(874, 598, pascal, 6, 6, 50, 100, False, 1)
                 p1.score = placement_score
                 placement_score = p2.score
-                p2 = Player(774, 543, o, 6, 6, 50, 100, True, 2)
+                p2 = Player(774, 543, scottie, 6, 6, 50, 100, True, 2)
                 p2.score = placement_score
                 p1.player_movement = 1
                 p2.player_movement = 2
             if p1.possession_change:
                 p2.reset_possession = False
                 placement_score = p1.score
-                p1 = Player(874, 598, x, 6, 6, 50, 100, False, 1)
+                p1 = Player(874, 598, pascal, 6, 6, 50, 100, False, 1)
                 p1.score = placement_score
                 placement_score = p2.score
-                p2 = Player(774, 543, o, 6, 6, 50, 100, True, 2)
+                p2 = Player(774, 543, scottie, 6, 6, 50, 100, True, 2)
                 p2.score = placement_score
                 win.blit(writing, (200, 500))
                 time.sleep(1)
             if p2.possession_change:
                 p1.reset_possession = False
                 placement_score = p1.score
-                p1 = Player(774, 598, x, 6, 6, 50, 100, True, 1)
+                p1 = Player(774, 598, pascal, 6, 6, 50, 100, True, 1)
                 p1.score = placement_score
                 placement_score = p2.score
-                p2 = Player(874, 543, o, 6, 6, 50, 100, False, 2)
+                p2 = Player(874, 543, scottie, 6, 6, 50, 100, False, 2)
                 p2.score = placement_score
                 win.blit(writing, (200, 500))
                 time.sleep(1)
 
-            if p1.score == win_score:
+            if p1.score >= win_score:
                 win.blit(court, (0, 0))
-                win.blit(x, (850, 140))
+                win.blit(pascal, (850, 140))
                 win.blit(p1_game_win_text, (850, 100))
                 pygame.display.update()
                 pygame.display.flip()
@@ -205,9 +221,9 @@ def main():
                 p2.score = 0
                 in_game = False
                 in_options = True
-            if p2.score == win_score:
+            if p2.score >= win_score:
                 win.blit(court, (0, 0))
-                win.blit(o, (850, 140))
+                win.blit(scottie, (850, 140))
                 win.blit(p2_game_win_text, (850, 100))
                 time.sleep(5)
                 p1.score = 0
